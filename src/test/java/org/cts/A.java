a1265bcea1bfbd9adb09adb68bd9086435cf2533
Latha202020 <lathaveeramani12@gmail.com>
2020-05-24 21:50:25 +0530

    Latha code is completed

--- src/test/java/org/cts/A.java
+++ src/test/java/org/cts/A.java
@@ -3,6 +3,7 @@ package org.cts;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("First Project");
+		System.out.println("First Project-In that 1st task completed ");
 	}
 
 }
